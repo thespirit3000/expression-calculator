89a2e435e9662c6c8814fee5ec84aec79141ca06
DzianisBogdan <thespirit3000@gmail.com>
2020-03-21 02:23:35 +0300

    feat: completion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function eval() {
     // Do not use eval!!!
     return;
 }
+const signs = ['/', '*', '-', '+'];
 
 const operators = {
     '/': function(a,b) {
@@ -14,19 +15,49 @@ const operators = {
     '+': function(a,b) {return a+b},
 }
 
-const signs = ['/', '*', '-', '+']
+const formatExpression = (expr) => {
+  expr = expr.split(' ').join('');
+  expr = expr.split('+').join(' + ');
+  expr = expr.split('-').join(' - ');
+  expr = expr.split('*').join(' * ');
+  expr = expr.split('(').join(' ( ');
+  expr = expr.split(')').join(' ) ');
+  expr = expr.split('/').join(' / ');
+  return expr;
+}
+
+const pairedBrackets = (expr) => {  
+  let count = 0;
+  let prevElement = '';
+  let exprArray = formatExpression(expr).split(' ');
+  exprArray.forEach(element => {
+    if (element === '('){
+      prevElement = '(';
+      count += 1;
+    }else{
+      if (element === ")"){
+        count -=1;
+      }
+    }
+  })
+  if (count !== 0 ){
+    throw new Error('ExpressionError: Brackets must be paired');
+  }
+}
+
+function calc(exprArray){
+  const endBracket = exprArray.indexOf(')');
+  for (let index = endBracket - 1; index > 0; index--) {
+    if(exprArray[index] === '(') {return index};
+  }
+}
+
+
 
 function expressionCalculator(expr) {
-    expr = expr.split(' ').join('')
-    console.log(expr);
-    expr = expr.split('+').join(' + ')
-    console.log(expr);
-    expr = expr.split('-').join(' - ')
-    console.log(expr);
-    expr = expr.split('*').join(' * ')
-    console.log(expr);
-    let exprArray = expr.split('/').join(' / ').split(' ');
-    console.log(exprArray);
+  pairedBrackets(expr);
+  
+  let exprArray = formatExpression(expr).split(' ');
     for (let index = 0; index < 4; ) {
         let sign = exprArray.indexOf(signs[index]);
         if(sign > 0){
