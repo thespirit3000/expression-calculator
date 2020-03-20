68f192245b6339b744e99d9243415d502a63dd2c
DzianisBogdan <thespirit3000@gmail.com>
2020-03-21 00:12:08 +0300

    feat: completion

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+const operators = {
+    '/': function(a,b) {return a/b},
+    '*': function(a,b) {return a*b},
+    '-': function(a,b) {return a-b},
+    '+': function(a,b) {return a+b},
 }
+
+const signs = ['/', '*', '-', '+']
+
+function expressionCalculator(expr) {
+    expr = expr.replace(/\s/g,'')
+    expr = expr.replace('+',' + ');
+    expr = expr.replace('-',' - ');
+    expr = expr.replace('/',' / ');
+    let exprArray = expr.replace('*',' * ').trim().split(' ');
+    console.log(exprArray);
+    for (let index = 0; index < 4; ) {
+        let sign = exprArray.indexOf(signs[index]);
+        if(sign > 0){
+            exprArray[sign - 1] = operators[exprArray[sign]](+exprArray[sign-1], +exprArray[sign+1]);
+            exprArray.splice(sign, 2);
+        }else{
+            index += 1;
+        }
+    }
+    return exprArray[0]
+}
+
 
 module.exports = {
     expressionCalculator
