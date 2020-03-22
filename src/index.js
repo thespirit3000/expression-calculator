f6ad3eb489f01fa7523ae63beb9e1ad1ee17bbdc
DzianisBogdan <thespirit3000@gmail.com>
2020-03-22 21:05:26 +0300

    refactor: code simplify

--- src/index.js
+++ src/index.js
@@ -88,16 +88,7 @@ function expressionCalculator(expr) {
       }
     }
   } else {
-    for (let index = 0; index < 4;) {
-      let sign = exprArray.indexOf(signs[index]);
-      if (sign > 0) {
-        exprArray[sign - 1] = operators[exprArray[sign]](+exprArray[sign - 1], +exprArray[sign + 1]);
-        exprArray.splice(sign, 2);
-      } else {
-        index += 1;
-      }
-    }
-    return exprArray[0]
+    return calculate(exprArray);
   }
 }
 module.exports = {
